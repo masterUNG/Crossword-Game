827af664e6ac25fc7b5693b9acb56b5afb00a5e8
masterUNG <phrombutr@gmail.com>
2016-11-23 14:53:52 +0700

    Intent to HowTo finish

--- app/src/main/java/rtc/patcharee/unchittha/crosswordgame/MainActivity.java
+++ app/src/main/java/rtc/patcharee/unchittha/crosswordgame/MainActivity.java
@@ -1,7 +1,10 @@
 package rtc.patcharee.unchittha.crosswordgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        //Image Controller
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, HowToActivity.class));
+            }
+        });
+
+    }   // Main Method
+
+}   // Main Class
